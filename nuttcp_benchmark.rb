90b0a3ac7d15004018d418fd4f5d1a8b85f8fa8f
Hiroki Kashiwazaki <reo.kashiwazaki@gmail.com>
2020-05-07 18:05:35 +0900

    alter nuttcp_benchmark.rb, config.yaml

--- nuttcp_benchmark.rb
+++ nuttcp_benchmark.rb
@@ -22,30 +22,42 @@ def which(command)
   end
 end
 
+def which_remotehost(command, remotehost)
+  if which(command["ssh"])
+    Open3.popen3(command["ssh"] + " " + remotehost + " which "+command) do |stdin, stdout, stderr, status|
+      if status.value.to_i == 0
+        return stdout.gets.strip
+      else
+        p command+" is not installed."
+        exit(1)
+      end
+    end
+  end
+end
+
 def detect_os
   return "redhat" if File.exist?("/etc/redhat-release")
   return "ubuntu" if File.exist?("/etc/lsb-release")
   return "debian" if File.exist?("/etc/debian_version")
 end
 
-def make_commands
+def make_commands(remotehost)
   required_commands = ["ip", "lscpu", "sudo", "sysctl", "ssh", "killall", "nuttcp"]
   required_commands.push("cpufreq-set") if detect_os == "ubuntu"
   required_commands.push("cpupower") if detect_os == "redhat"
 
   commands = Hash.new
   required_commands.each{|command|
-    p command
     commands[command] = which(command)
+    commands[command + "_remote"] = which_remote(command, remotehost)
   }
   return command
 end
 
-def link_mtu(commands, link)
-  command = commands["ip"] + " link show " + link
+def exec_command(command)
   Open3.popen3() do |stdin, stdout, stderr, status|
     if status.value.to_i == 0
-      return stdout.gets.strip.split(' ')[5]
+      return stdout
     else
       p stderr
       exit(1)
@@ -53,8 +65,30 @@ def link_mtu(commands, link)
   end
 end
 
+def link_mtu(commands, link)
+  command = commands["ip"] + " link show " + return
+  link exec_command(command).gets.strip.split(' ')[5]
+end
+
+def link_mtu_remotehost(commands, link, remotehost)
+  command = commands["ip"] + " link show " + link
+  return exec_command(command).gets.strip.split(' ')[5]
+end
+
+
+def set_link_mtu(commands, link, mtu)
+  command = commands["sudo"] + " " + commands["ip"] + " link set " + link + " mtu " + mtu
+  return exec_command(command)
+end
+
+target_link = conf["target_link"]
+target_remotehost = conf["target_remotehost"]
 commands = make_commands
-p link_mtu(commands, conf["link"])
-conf["target"].each{|mtu|
-  
+initial_mtu = link_mtu(commands, target_link)
+conf["target_mtu"].each{|mtu|
+  set_link_mtu(commands, target_link, mtu)
+  killall_nuttcp_remotehost(target_remotehost)
 }
+
+# initialized
+set_link_mtu(commands, target_link, initial_mtu)
